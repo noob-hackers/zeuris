83976e852f705e46bed2539c8b8752e1f1b5aa13
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2025-01-30 14:20:09 +0530

    Update zeuris.sh

--- zeuris.sh
+++ zeuris.sh
@@ -1,4 +1,5 @@
 #Tool By Noob Hackers
+echo -e "\e[33m                  Wait For 5 Mins...!"
 pkg install ncurses-utils -y > /dev/null 2>&1 &
 pkg install chafa -y > /dev/null 2>&1 &
 ssh -o StrictHostKeyChecking=no -R 80:localhost:8013 nokey@localhost.run > /dev/null 2>&1
